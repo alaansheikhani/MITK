a221a111207ecaba543b935486f78aeca16da75e
Nami Tracking Notebook <franz@hs-ulm.de>
2020-02-18 16:49:42 +0100

    First working version with fixed setup

--- Modules/IGT/DataManagement/mitkTrackingDeviceSource.cpp
+++ Modules/IGT/DataManagement/mitkTrackingDeviceSource.cpp
@@ -22,6 +22,14 @@ See LICENSE.txt or http://www.mitk.org for details.
 #include "mitkIGTTimeStamp.h"
 #include "mitkIGTException.h"
 #include "mitkIGTHardwareException.h"
+#include <mitkNavigationDataLandmarkTransformFilter.h>
+
+#include <vtkLandmarkTransform.h>
+#include <vtkSmartPointer.h>
+#include <vtkTransform.h>
+#include <vtkTransformPolyDataFilter.h>
+#include <vtkAppendPolyData.h>
+#include <vtkPoints.h>
 
 mitk::TrackingDeviceSource::TrackingDeviceSource()
   : mitk::NavigationDataSource(), m_TrackingDevice(nullptr)
@@ -64,6 +72,44 @@ void mitk::TrackingDeviceSource::GenerateData()
       << m_TrackingDevice->GetToolCount() << " tools available in the tracking device.";
     throw std::out_of_range(ss.str());
   }
+
+  // Declare needed Variables for Landmark-Transformation
+
+  //mitk::NavigationDataLandmarkTransformFilter::Pointer m_LandmarkTransformFilter = mitk::NavigationDataLandmarkTransformFilter::New();
+  //mitk::PointSet::Pointer m_SourcePointSet = mitk::PointSet::New();
+  //mitk::PointSet::Pointer m_TargetPointSet = mitk::PointSet::New();
+
+  //mitk::PointSet::PointType pointonVirtual;
+  //m_SourcePointSet->GetPointIfExists(0, &pointonVirtual);
+  //mitk::NavigationData::Pointer m_virtualModelInputND = mitk::NavigationData::New();
+  //m_virtualModelInputND->SetPosition(pointonVirtual);
+  //m_virtualModelInputND->Modified();
+
+  vtkSmartPointer<vtkPoints> sourcePoints = vtkSmartPointer<vtkPoints>::New();
+  vtkSmartPointer<vtkPoints> targetPoints = vtkSmartPointer<vtkPoints>::New();
+
+
+  //Define and Insert SourcePoints
+  // mitk::NavigationData::PositionType srcPt1;
+  // mitk::Point3D srcPt1;
+  // srcPt1[0] = 0;
+  // srcPt1[1] = 20;
+  // srcPt1[2] = 0;
+  double srcPt1[3] = {0, 20, 0};
+  double srcPt2[3] = {0, 0, 0};
+  double srcPt3[3] = {20, 0, 0};
+  double srcPt4[3] = {40, 0, 0};
+
+  //m_SourcePointSet->InsertPoint(srcPt1);
+  //m_SourcePointSet->InsertPoint(srcPt2);
+  //m_SourcePointSet->InsertPoint(srcPt3);
+  //m_SourcePointSet->InsertPoint(srcPt4);
+  sourcePoints->InsertNextPoint(srcPt1);
+  sourcePoints->InsertNextPoint(srcPt2);
+  sourcePoints->InsertNextPoint(srcPt3);
+  sourcePoints->InsertNextPoint(srcPt4);
+
+
   /* update outputs with tracking data from tools */
   unsigned int toolCount = m_TrackingDevice->GetToolCount();
   for (unsigned int i = 0; i < toolCount; ++i)
@@ -80,20 +126,144 @@ void mitk::TrackingDeviceSource::GenerateData()
     }
     nd->SetDataValid(true);
     mitk::NavigationData::PositionType p;
+    mitk::NavigationData::OrientationType o;
     t->GetPosition(p);
     nd->SetPosition(p);
-
-    mitk::NavigationData::OrientationType o;
     t->GetOrientation(o);
     nd->SetOrientation(o);
     nd->SetOrientationAccuracy(t->GetTrackingError());
     nd->SetPositionAccuracy(t->GetTrackingError());
     nd->SetIGTTimeStamp(t->GetIGTTimeStamp());
 
+    if (i == 0)
+    {
+      double targetPoint1[3] = {0, 0, 0};
+      mitk::NavigationData::Pointer trgPt1 = mitk::NavigationData::New();
+      trgPt1->SetPosition(targetPoint1);
+      trgPt1->Compose(nd);
+
+      double targetPoint2[3] = {0, 0, 20};
+      mitk::NavigationData::Pointer trgPt2 = mitk::NavigationData::New();
+      trgPt2->SetPosition(targetPoint2);
+      trgPt2->Compose(nd);
+
+      //m_TargetPointSet->InsertPoint(trgPt1->GetPosition());
+      //m_TargetPointSet->InsertPoint(trgPt2->GetPosition());
+      mitk::Point3D t1 = trgPt1->GetPosition();
+      double targetPointComp[3] = {t1[0], t1[1], t1[2]};
+      targetPoints->InsertNextPoint(targetPointComp);
+
+      mitk::Point3D t2 = trgPt2->GetPosition();
+      double targetPointComp2[3] = {t2[0], t2[1], t2[2]};
+      targetPoints->InsertNextPoint(targetPointComp2);
+    }
+    if (i == 1)
+    {
+      double targetPoint3[3] = {0, 0, 0};
+      mitk::NavigationData::Pointer trgPt3 = mitk::NavigationData::New();
+      trgPt3->SetPosition(targetPoint3);
+      trgPt3->Compose(nd);
+      //trgPt1->Compose(this->GetOutput(0));
+
+
+      mitk::Point3D t3 = trgPt3->GetPosition();
+      double targetPointComp3[3] = {t3[0], t3[1], t3[2]};
+      targetPoints->InsertNextPoint(targetPointComp3);
+
+      double targetPoint4[3] = {0, 0, 20};
+
+      mitk::NavigationData::Pointer trgPt4 = mitk::NavigationData::New();
+      trgPt4->SetPosition(targetPoint4);
+      trgPt4->Compose(nd);
+
+      //m_TargetPointSet->InsertPoint(trgPt3->GetPosition());
+      //m_TargetPointSet->InsertPoint(trgPt4->GetPosition());
+
+      mitk::Point3D t4 = trgPt4->GetPosition();
+      double targetPointComp4[4] = {t4[0], t4[1], t4[2]};
+      targetPoints->InsertNextPoint(targetPointComp4);
+
+      //m_LandmarkTransformFilter->SetSourceLandmarks(m_SourcePointSet);
+      //m_LandmarkTransformFilter->SetTargetLandmarks(m_TargetPointSet);
+      //m_LandmarkTransformFilter->SetInput(0, m_virtualModelInputND);
+      //m_LandmarkTransformFilter->Update();
+      //m_LandmarkTransformFilter->GetOutput(0);
+
+      // compute transform
+      vtkSmartPointer<vtkLandmarkTransform> transform = vtkSmartPointer<vtkLandmarkTransform>::New();
+      transform->SetSourceLandmarks(sourcePoints);
+      transform->SetTargetLandmarks(targetPoints);
+      transform->SetModeToRigidBody();
+      transform->Modified();
+      transform->Update();
+
+      // convert from vtk to itk data types
+      itk::Matrix<float, 3, 3> rotationFloat = itk::Matrix<float, 3, 3>();
+      itk::Vector<float, 3> translationFloat = itk::Vector<float, 3>();
+      itk::Matrix<double, 3, 3> rotationDouble = itk::Matrix<double, 3, 3>();
+      itk::Vector<double, 3> translationDouble = itk::Vector<double, 3>();
+      vtkSmartPointer<vtkMatrix4x4> m = transform->GetMatrix();
+      for (int k = 0; k < 3; k++)
+        for (int l = 0; l < 3; l++)
+        {
+          rotationFloat[k][l] = m->GetElement(k, l);
+          rotationDouble[k][l] = m->GetElement(k, l);
+        }
+      for (int k = 0; k < 3; k++)
+      {
+        translationFloat[k] = m->GetElement(k, 3);
+        translationDouble[k] = m->GetElement(k, 3);
+      }
+      // create affine transform 3D surface
+      mitk::AffineTransform3D::Pointer mitkTransform = mitk::AffineTransform3D::New();
+      mitkTransform->SetMatrix(rotationDouble);
+      mitkTransform->SetOffset(translationDouble);
+      //#############################################################################################
+
+      //############### object is transformed ##########################
+      // save transform
+      mitk::NavigationData::Pointer transformedND = mitk::NavigationData::New(mitkTransform); // this is stored in a member because it is needed for permanent registration later on
+      transformedND->SetName("Test6D");
+      transformedND->SetDataValid(true);
+      this->GetOutput(i)->Graft(transformedND);
+
+     //mitk::NavigationData::Pointer transformedND = mitk::NavigationData::New();
+     //nd = transformedND;
+     //nd->SetPosition(transformedND->GetPosition());
+     //nd->SetOrientation(transformedND->GetOrientation());
+
+     }
+
+    //nd->SetPosition(p);
+
+    //mitk::NavigationData::OrientationType o;
+    //t->GetOrientation(o);
+    //nd->SetOrientation(o);
+    ////nd->SetOrientationAccuracy(t->GetTrackingError());
+    ////nd->SetPositionAccuracy(t->GetTrackingError());
+    ////nd->SetIGTTimeStamp(t->GetIGTTimeStamp());
+
     //for backward compatibility: check if the timestamp was set, if not create a default timestamp
-    if (nd->GetIGTTimeStamp()==0) nd->SetIGTTimeStamp(mitk::IGTTimeStamp::GetInstance()->GetElapsed());
+    //if (nd->GetIGTTimeStamp()==0) nd->SetIGTTimeStamp(mitk::IGTTimeStamp::GetInstance()->GetElapsed());
+
   }
+
+  //mitk::NavigationData::Pointer test1 = mitk::NavigationData::New();
+  //mitk::NavigationData::Pointer test2 = mitk::NavigationData::New();
+  //test1->SetName("Test");
+  //test1->SetDataValid(true);
+  //this->GetOutput(0)->Graft(test1);
+  //this->GetOutput(1)->Graft(test2);
+  //double point[3] = {0, 0, 0};
+  /*
+  test1->SetPosition(point);
+  test1->SetDataValid(true);
+  test2->SetPosition(point);
+
+  this->SetNthOutput(0, test1);
+  this->SetNthOutput(1, test2);*/
 }
+
 
 void mitk::TrackingDeviceSource::SetTrackingDevice( mitk::TrackingDevice* td )
 {
